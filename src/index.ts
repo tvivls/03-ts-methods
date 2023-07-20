c226136de8199fd16cd5f656e4a38ac29b897eba
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-20 15:17:18 +0300

    fix (index.ts): вынесены методы и добавлен вызов

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,14 @@
-type VisibilityType = 'visible' | 'hidden';
+import {hidden, visibilityChange, visible} from './visibility';
 
-const buildPromise = (isVisible: VisibilityType | boolean) => new Promise<boolean>((resolve) => {
-    const handleVisibility = () => {
-        document.removeEventListener('visibilitychange', handleVisibility);
-        switch (typeof isVisible) {
-            case 'boolean':
-                resolve(document.visibilityState === 'visible');
-                break;
-            case 'string':
-                resolve(document.visibilityState === isVisible);
-                break;
-        }
-    };
-    document.addEventListener('visibilitychange', handleVisibility);
-});
 
-export const visible = () => {
-    if (document.visibilityState === 'visible')
-        return Promise.resolve(true);
-    return buildPromise('visible');
-};
-
-export const hidden = () => {
-    if (document.visibilityState === 'hidden')
-        return Promise.resolve(true);
-    return buildPromise('hidden');
-};
-
-export const visibilityChange = () => buildPromise(document.visibilityState === 'visible');
-
-const asd = async (description: string, visibilityFn: () => Promise<boolean>) => {
-    // const returned = await visible();
-    // const gone = await hidden();
+const displayResult = async (description: string, visibilityFn: () => Promise<boolean>) => {
     const visibilityStatus = await visibilityFn();
     const body = document.querySelector('body');
     const text = document.createElement('p');
-    text.innerText = `${description} ${visibilityStatus}`;
+    text.innerText = `${description}: ${visibilityStatus}`;
     body?.appendChild(text);
 };
 
-asd('visibilityChange function:', visibilityChange);
-asd('visible function:', visible);
-asd('hidden function:', hidden);
-
+displayResult('visibility change', visibilityChange);
+displayResult('the user is here', visible);
+displayResult('the user was leaving', hidden);
